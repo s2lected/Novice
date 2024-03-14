ee74e222aaaeabe1b0b8e6ae1a974b8c631c4a81
maison <161265371+s2lected@users.noreply.github.com>
2024-03-14 15:06:17 +0900

    Update 082. 3 6 9 게임의 왕이 되자.py

--- Python 기초 100제/082. 3 6 9 게임의 왕이 되자.py
+++ Python 기초 100제/082. 3 6 9 게임의 왕이 되자.py
@@ -2,7 +2,11 @@ n = int(input())
 my_list = []
 
 for i in range(1,n + 1):
-    if i % 3 == 0:
+    if i % 10 == 3:
+        my_list.append('X')
+    elif i % 10 == 6:
+        my_list.append('X')
+    elif i % 10 == 9:
         my_list.append('X')
     else:
         my_list.append(str(i))
