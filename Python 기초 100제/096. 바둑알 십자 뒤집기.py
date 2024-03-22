464db36f5a78758748310a14dfaa090d99b1de74
maison <161265371+s2lected@users.noreply.github.com>
2024-03-22 14:04:30 +0900

    Create 096. 바둑알 십자 뒤집기.py

---
+++ Python 기초 100제/096. 바둑알 십자 뒤집기.py
@@ -0,0 +1,31 @@
+d=[]
+for i in range(20) :
+  d.append([])
+  for j in range(20) : 
+    d[i].append(0)
+ 
+for i in range(19) :
+  a = input().split()
+  for j in range(19) :
+    d[i+1][j+1] = int(a[j])
+ 
+n = int(input())
+for i in range(n) :
+  x,y=input().split()
+  x=int(x)
+  y=int(y)
+  for j in range(1, 20) :
+    if d[j][y]==0 :
+      d[j][y]=1
+    else :
+      d[j][y]=0
+ 
+    if d[x][j]==0 :
+      d[x][j]=1
+    else :
+      d[x][j]=0
+ 
+for i in range(1, 20) :
+  for j in range(1, 20) :
+    print(d[i][j], end=' ')
+  print()
